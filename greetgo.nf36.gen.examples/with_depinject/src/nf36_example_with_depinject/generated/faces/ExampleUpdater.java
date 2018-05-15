e543ff801867c5752f5c70b2b7f219ada30d0027
ekolpakov <ekolpakov@greet-go.com>
2018-05-15 10:51:22 +0600

    Killed left files

--- greetgo.nf36.gen.examples/with_depinject/src/nf36_example_with_depinject/generated/faces/ExampleUpdater.java
+++
@@ -1,7 +0,0 @@
-package nf36_example_with_depinject.generated.faces;
-
-
-
-public interface ExampleUpdater {
-
-}
--- greetgo.nf36.gen.examples/with_depinject/src/nf36_example_with_depinject/generated/impl/AbstractExampleUpdater.java
+++
@@ -1,7 +0,0 @@
-package nf36_example_with_depinject.generated.impl;
-
-import nf36_example_with_depinject.generated.faces.ExampleUpdater;
-
-public abstract class AbstractExampleUpdater implements ExampleUpdater {
-
-}
