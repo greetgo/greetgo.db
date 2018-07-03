f5265f5a49c96350ddca5cee5119fcc4e3f69a95
ekolpakov <ekolpakov@greet-go.com>
2018-07-03 17:52:12 +0600

    Добавлено логирование SequenceNext

--- greetgo.nf36/src/kz/greetgo/db/nf36/adapters/JdbcSequenceNextAbstractAdapter.java
+++ greetgo.nf36/src/kz/greetgo/db/nf36/adapters/JdbcSequenceNextAbstractAdapter.java
@@ -3,10 +3,12 @@ package kz.greetgo.db.nf36.adapters;
 import kz.greetgo.db.Jdbc;
 import kz.greetgo.db.nf36.core.SequenceNext;
 import kz.greetgo.db.nf36.core.SqlLogAcceptor;
+import kz.greetgo.db.nf36.model.SqlLog;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 abstract class JdbcSequenceNextAbstractAdapter implements SequenceNext {
   Jdbc jdbc;
@@ -28,13 +30,32 @@ abstract class JdbcSequenceNextAbstractAdapter implements SequenceNext {
 
   public <T> T next(String sequenceName, Taker<T> taker) {
     return jdbc.execute(con -> {
-      try (PreparedStatement ps = con.prepareStatement(selectSequence(sequenceName))) {
+      long startedAt = System.nanoTime();
+      String sql = selectSequence(sequenceName);
+      try (PreparedStatement ps = con.prepareStatement(sql)) {
         try (ResultSet rs = ps.executeQuery()) {
           if (!rs.next()) throw new RuntimeException("Left error");
-          return taker.take(rs);
+          T ret = taker.take(rs);
+          logInfo(startedAt, sql);
+          return ret;
         }
+      } catch (Exception e) {
+        throw logError(startedAt, sql, e);
       }
     });
+  }
+
+  private Exception logError(long startedAt, String sql, Exception e) {
+    if (logAcceptor != null && logAcceptor.isErrorEnabled()) {
+      logAcceptor.accept(new SqlLog(sql, new ArrayList<>(), e, System.nanoTime() - startedAt));
+    }
+    return e;
+  }
+
+  private void logInfo(long startedAt, String sql) {
+    if (logAcceptor != null && logAcceptor.isTraceEnabled()) {
+      logAcceptor.accept(new SqlLog(sql, new ArrayList<>(), null, System.nanoTime() - startedAt));
+    }
   }
 
   protected abstract String selectSequence(String sequenceName);
