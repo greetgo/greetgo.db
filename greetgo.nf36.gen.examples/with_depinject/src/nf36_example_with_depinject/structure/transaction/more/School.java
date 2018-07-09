29e50ce1f2365b6b681c9d4fdc2bd11a66603b5c
ekolpakov <ekolpakov@greet-go.com>
2018-07-09 14:03:52 +0600

    Проверена генерация SQL

--- greetgo.nf36.gen.examples/with_depinject/src/nf36_example_with_depinject/structure/transaction/more/School.java
+++ greetgo.nf36.gen.examples/with_depinject/src/nf36_example_with_depinject/structure/transaction/more/School.java
@@ -1,13 +1,18 @@
 package nf36_example_with_depinject.structure.transaction.more;
 
+import kz.greetgo.db.nf36.core.Nf3Description;
 import kz.greetgo.db.nf36.core.Nf3Entity;
 import kz.greetgo.db.nf36.core.Nf3ID;
 
-@SuppressWarnings("unused")
 @Nf3Entity
+@SuppressWarnings("unused")
+@Nf3Description("Description")
 public class School {
+
   @Nf3ID
+  @Nf3Description("Description")
   public String id;
 
+  @Nf3Description("Description")
   public String name;
 }
