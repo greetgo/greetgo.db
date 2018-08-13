627f4d49e1d244818b3c2b4e9d6c1f749f60f5db
ekolpakov <ekolpakov@greet-go.com>
2018-08-13 11:11:16 +0600

    Added EntityEnumAsId

---
+++ greetgo.nf36.gen.examples/with_depinject/src/nf36_example_with_depinject/structure/EntityEnumAsId.java
@@ -0,0 +1,16 @@
+package nf36_example_with_depinject.structure;
+
+import kz.greetgo.db.nf36.core.Nf3Description;
+import kz.greetgo.db.nf36.core.Nf3Entity;
+import kz.greetgo.db.nf36.core.Nf3ID;
+
+@Nf3Entity
+@Nf3Description("Энтря с ИД типа Enum")
+public class EntityEnumAsId {
+  @Nf3ID
+  @Nf3Description("enum id")
+  public SomeEnum id;
+
+  @Nf3Description("some value")
+  public String value;
+}
